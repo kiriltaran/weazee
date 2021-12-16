9dbcc8737b0c9756ede3c28f90031cca1c169a10
Vlad Sosnov <vladsosnov2001@gmail.com>
2021-12-16 19:32:20 +0200

    feat(components): use correct icons for weazee properties component (#22)

--- src/components/WeazeeProperties/WeazeeProperties.tsx
+++ src/components/WeazeeProperties/WeazeeProperties.tsx
@@ -1,13 +1,16 @@
 import type { FC } from 'react';
 import styles from './WeazeeProperties.module.scss';
 import { WeazeePropertiesItem } from 'components/WeazeePropertiesItem';
-import { Fog, Rain, Breezy } from 'icons';
+import { Humidity, AirPressure, Rain, Breezy } from 'icons';
 
 export const WeazeeProperties: FC = () => {
-  // TODO: fix icons for Humidity and Air Pressure
   const properties = [
-    { icon: <Fog width={34} />, title: 'Humidity', value: '50 %' },
-    { icon: <Fog width={34} />, title: 'Air Pressure', value: '1009.483 PS' },
+    { icon: <Humidity width={34} />, title: 'Humidity', value: '50 %' },
+    {
+      icon: <AirPressure width={34} />,
+      title: 'Air Pressure',
+      value: '1009.483 PS',
+    },
     { icon: <Rain width={34} />, title: 'Chance of Rain', value: '0 %' },
     { icon: <Breezy width={34} />, title: 'Wind Speed', value: '1.4 km/h' },
   ];
